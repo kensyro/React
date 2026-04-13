ffedac202d8b7d411f6d6007786a41e74b296640
huythanhit <huyltse184712@fpt.edu.vn>
2026-04-13 23:34:17 +0700

    feat: get-todolist and create add-todolist

--- react-todolist/src/App.tsx
+++ react-todolist/src/App.tsx
@@ -1,121 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+type JsonToDoList = {
+  id: number,
+  title: string
 }
 
-export default App
+const jsonToDoList: Array<JsonToDoList> = [
+  {
+    id: 1,
+    title: "Học bài"
+  },
+  {
+    id: 2,
+    title: "Đi chơi"
+  },
+  {
+    id: 3,
+    title: "Nấu cơm"
+  }
+]
+
+function App() {
+    const [todoList, setTodoList] = useState(jsonToDoList);
+    const [newToDo, setNewtoDo ] = useState<string>("");
+    return (
+      <>
+      <form onSubmit={(event) => {
+          event.preventDefault();
+          const newList = todoList.concat([
+            {title: newToDo, id: Math.random()},
+          ]);
+          setTodoList(newList);
+          setNewtoDo(""); 
+      }}>
+        
+        <input          
+          type="text"
+          value={newToDo} 
+          onChange={(event) => {
+            event.target.value;
+            console.log("Value", event.target.value);
+            setNewtoDo(event.target.value);
+          }}
+        />
+        <button type='submit'>Add To Do</button>
+      </form>
+      {
+        todoList.map((todoItem) => {
+          return (
+            <form 
+            style={{
+              display: "flex",
+              gap: "50px",
+            }}
+            key={todoItem.id}
+            >
+            <p>{todoItem.title}</p>  
+            </form>
+          )
+        })
+      }
+      </>
+    )
+}
+
+export default App;
