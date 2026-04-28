d2d3b36660b7810f42d219a135690f986ca41d44
huythanhit <huyltse184712@fpt.edu.vn>
2026-04-28 17:20:15 +0700

    feat: movement snake and setup checkpoint, button reset , loser

--- root/webgame/src/app/page.tsx
+++ root/webgame/src/app/page.tsx
@@ -1,51 +1,159 @@
 "use client";
 import { useEffect, useState } from "react";
+import { ArrowLeft, ArrowRight, ArrowDown, ArrowUp, Play } from 'lucide-react';
 
 export default function Home() {
   const count = 20;
-  const [config, setConfig] = useState(false);
   const [snake, setSnake] = useState([
-    {x: 3, y: 1},
-    {x: 4, y: 1},
-    {x: 5, y: 1},
-    {x: 6, y: 1},
+    {x: 10, y: 1},
+    {x: 11, y: 1},
+    {x: 12, y: 1},
+    {x: 13, y: 1},
   ]);
-  const [map,setMap] = useState({x:-1, y:0})
+  const [checkpoint, setCheckPoint] = useState({x:10, y:12});
+  const [map,setMap] = useState({x:0, y:1})
+  const [isOver, setIsOver] = useState(false);
+  
+  type SymmetryType = {
+    x: number,
+    y: number
+  };
+  
   useEffect(() => {
+    if (isOver) return;
     const interval = setInterval(() => {
       setSnake((prev) => {
         const head = prev[0];
         const newHead = { x: head.x + map.x, y: head.y + map.y };
-
+        if(newHead.x < 0 || newHead.x >= count ||
+          newHead.y < 0 || newHead.y >= count
+        ) {
+          setIsOver(true);
+          // alert("Bạn đã thua");
+          return prev;
+        }
+        if(prev.some(s=> s.x === newHead.x && s.y === newHead.y)) {
+          setIsOver(true);
+          return prev;
+        }
         const next = [newHead, ...prev];
-        next.pop();
+        // checkpoint 
+        if (newHead.x === checkpoint.x && newHead.y === checkpoint.y) {
+          let newPoint: {
+          x: number,
+          y: number
+          };
+          let isOnSnake;
+          do {
+            newPoint = {
+              x: Math.floor(Math.random() * count),
+              y: Math.floor(Math.random() * count),
+            };
+
+            isOnSnake = next.some(
+              (s) => s.x === newPoint.x && s.y === newPoint.y
+            );
+          } while (isOnSnake);
+          setCheckPoint(newPoint);
+        } else {
+          next.pop();
+        }
         return next;
       });
-    }, 100);
+    }, 800);
+    return () => clearInterval(interval);
+  }, [map, checkpoint, isOver])
 
-    return () => clearInterval(interval)
-  }, [map])
-
-  const getTypeBoard = (x: number, y: number) => {
-    if (snake[0].x === x && snake[0].y === y) return "head";
-    if (snake.some((s) => s.x === x && s.y === y)) return "body";
-    return "empty";
+  const getBoard = (x: number, y: number) => {
+    if (snake[0].x === x && snake[0].y === y) {
+      return "head";
+    }
+    if (snake.some((s) => s.x === x && s.y === y)) {
+    return "body";
+    }
+    if (checkpoint.x === x && checkpoint.y === y) {
+      return "checkpoint";
+    }     
   };
+  const handleStart = () => {
+    // công thức: random * (max - min) + min
+    const x = Math.floor(Math.random() * (15 - 5) + 5)
+    const y = Math.floor(Math.random() * (15 - 5) + 5)
+    const isHorizontal = Math.random() < 0.5
+    const isCount = Math.random() < 0.5
+    const newSnake = [{x, y}]
+    const symmetryChance: SymmetryType = isHorizontal 
+                                       ? {x: isCount ? 1 : -1, y: 0 }
+                                       : {x: 0, y: isCount ? 1 : -1 }
+    for (let i = 0; i < 3; i ++) {
+        const head = newSnake[i];
+        const newHead = { x: head.x - symmetryChance.x, y: head.y - symmetryChance.y };
+        newSnake.push(newHead)  
+    }
+    let newPoint: {
+    x: number,
+    y: number
+    };
+    let isOnSnake;
+    do {
+      newPoint = {
+        x:Math.floor(Math.random()* count),
+        y:Math.floor(Math.random()* count),
+      };
+      isOnSnake = newSnake.some(
+        (s) => s.x === newPoint.x && s.y === newPoint.y
+      );
+    } while (isOnSnake);    
+    setMap(symmetryChance)
+    setSnake(newSnake)
+    setCheckPoint(newPoint)
+  }
 
+  // Handle and Use button UP DOWN LEFT RIGHT
+  const handleUp = () => {
+    if (map.y !== 1) {
+      setMap({x:0, y: -1})
+    }
+  }
+
+  const handleDown = () => {
+    if(map.y !== -1) {
+      setMap({x:0, y:1})
+    }
+  }
+
+  const handleLeft = () => {
+    if(map.x !== 1) {
+      setMap({x:-1, y:0})
+    }
+  }
+
+  const handleRight = () => {
+    if(map.x !== -1) {
+      setMap({x:1, y:0})
+    }
+  }
+  const handleReset = () => {
+    setIsOver(false);
+    handleStart();
+  }
   return (
-    <section className="flex justify-center items-center h-dvh">
-      <section className="w-[50vw] aspect-square border border-black">
+    <section className="flex flex-col justify-center items-center h-dvh">
+      <section className="w-[40vw] aspect-square border border-black">
         <div 
         className="grid" 
         style={{gridTemplateColumns : `repeat(${count},1fr)` }}>
           {
             Array.from({ length: count }).map((_v, key1) => {
               return Array.from({ length: count }).map((_v, key2) => {
-                const type = getTypeBoard(key2, key1);
+                const type = getBoard(key2, key1);
                 return (
                   <div
                     key={`matrix-${key1}-${key2}`}
-                    className={`w-full aspect-square border border-blue-300 ${type === 'head' ? 'bg-amber-300' : ''} ${type === 'body' ? 'bg-sky-300' : ''}`}
+                    className={`w-full aspect-square border border-blue-300 
+                    ${type === 'head' ? 'bg-amber-300' : ''} 
+                    ${type === 'body' ? 'bg-sky-300' : ''} 
+                    ${type === 'checkpoint'? 'bg-red-600!' : ''}`}
                   ></div>
                 );
               });
@@ -53,7 +161,21 @@ export default function Home() {
           }
         </div>
       </section>
-
-    </section>
+      <section className="flex gap-2">
+        <div className="flex justify-center items-center mt-3 border-2 w-10 h-10 " onClick={handleStart}><Play /></div>
+        <div className="flex justify-center items-center mt-3 border-2 w-10 h-10 " onClick={handleLeft}><ArrowLeft /></div>
+        <div className="flex justify-center items-center mt-3 border-2 w-10 h-10 " onClick={handleUp}><ArrowUp /></div>
+        <div className="flex justify-center items-center mt-3 border-2 w-10 h-10 " onClick={handleDown}><ArrowDown /></div>
+        <div className="flex justify-center items-center mt-3 border-2 w-10 h-10 " onClick={handleRight }><ArrowRight /></div>
+      </section>
+      {isOver && (
+        <div className= "fixed inset-0 flex justify-center items-center backdrop-blur-5px">
+          <div className=" text-white bg-red-500 border-2 border-amber-950 ">
+            Bạn đã thua !
+          <button className="border-2 border-amber-950 ml-2 text-white bg-blue-800 " onClick={handleReset}>Reset</button>
+          </div>
+        </div>
+      )}
+    </section> 
   );
 }
